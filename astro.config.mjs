50970c2a8fe1884b0f87be0fc37989df7abb150b
zyb <384201115@qq.com>
2025-01-24 22:36:39 +0800

    add yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  site: 'https://haowuyiwai.github.io',
   // base: '/docs',
   trailingSlash: 'never',
 
